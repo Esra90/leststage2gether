e5a48d6e457a9cf003c7b086c84ff38bc2fe50f0
Esra Ozel <esra.ozel_@hotmail.com>
2020-09-17 22:14:50 +0200

    User component created

---
+++ src/musicEvents/pages/NewEvent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const NewEvent = () => {
+    return (
+        <h1>Events works!</h1>
+    )
+}
+
+export default NewEvent;
---
+++ src/user/components/UsersList.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './UsersList.css';
+import UserItem from './UserItem';
+
+// used prop to get the list of the users
+const UsersList = props => {
+    // Check if there is any user to show
+    if(props.items.length === 0 ){
+        return (
+            <div className="center">
+                <h2>No users found</h2>
+            </div>
+        );
+    }
+    // If there is any user to show 
+    return <ul className="users-list">
+        {props.items.map(user => (
+            <UserItem 
+                key={user.id} 
+                id={user.id} 
+                image={user.image} 
+                name={user.name} 
+                eventCount={user.events} 
+            />
+        ))}
+    </ul>
+}
+
+
+export default UsersList;
+
+
+// import React from 'react';
+
+// import UserItem from './UserItem';
+// import './UsersList.css';
+
+// const UsersList = props => {
+//   if (props.items.length === 0) {
+//     return (
+//       <div className="center">
+//         <h2>No users found.</h2>
+//       </div>
+//     );
+//   }
+
+//   return (
+//     <ul className="users-list">
+//       {props.items.map(user => (
+//         <UserItem
+//           key={user.id}
+//           id={user.id}
+//           image={user.image}
+//           name={user.name}
+//           placeCount={user.places}
+//         />
+//       ))}
+//     </ul>
+//   );
+// };
+
+// export default UsersList;
